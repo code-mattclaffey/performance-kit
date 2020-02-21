80f810d105f181911a1d89e0d3203078aa0cbee3
Matthew Claffey <matthewclaffey@matthews-mbp.mynet>
2020-02-21 21:14:33 +0000

    chore: add content to descriptions

--- meta/index.js
+++ meta/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     title: 'Performance Kit',
-    description: 'My description',
+    description:
+        "This collection of tutorials are based on my learnings from client projects. For each topic I will show you how to identify, implement and evaluate performance techniques that can improve your site's load time.",
 };
