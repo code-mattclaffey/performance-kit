70b75d244bd52d32c15e9348de57da57024ce919
Matthew Claffey <matthewclaffey@matthews-mbp.mynet>
2020-02-23 22:56:30 +0000

    chore: update blog pages

--- config/blog-pages.js
+++ config/blog-pages.js
@@ -1,3 +1,3 @@
 
-        export const blogPages = [{"url":"/posts/hidden-images","name":"hidden images"},{"url":"/posts/lazy-loading-images","name":"lazy loading images"},{"url":"/posts/loading-fonts-the-fout-way","name":"loading fonts the fout way"},{"url":"/posts/responsive-images","name":"responsive images"}];
+        export const blogPages = [{"url":"/posts/hidden-images","name":"hidden images"},{"url":"/posts/image-compression","name":"image compression"},{"url":"/posts/lazy-loading-images","name":"lazy loading images"},{"url":"/posts/loading-fonts-the-fout-way","name":"loading fonts the fout way"},{"url":"/posts/responsive-images","name":"responsive images"}];
     
