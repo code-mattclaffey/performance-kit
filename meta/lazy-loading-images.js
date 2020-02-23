6c6de5dc72ca7576c511896b2be2a5ba7ae9bfc2
Matthew Claffey <matthewclaffey@matthews-mbp.mynet>
2020-02-23 06:16:14 +0000

    chore: update meta data

--- meta/lazy-loading-images.js
+++ meta/lazy-loading-images.js
@@ -1,5 +1,5 @@
 module.exports = {
     title: 'Performance Kit - Lazy Loading images',
     description:
-        'When the image is set to display: none; at a certain breakpoint, the image still gets loaded in the browser making the page weight bigger than what it needs to be.',
+        'Lazy loading is a great method to use for when you want to only load images in view i.e the fold. When you scroll through the page, images will load as they come into view.',
 };
