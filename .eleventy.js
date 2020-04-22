16fd9a348978040ac7016024ad503a9efeb06613
code-mattclaffey <mattclaffey93@gmail.com>
2020-04-22 17:39:38 +0100

    feat: add new service worker plugin and used critical css

--- .eleventy.js
+++ .eleventy.js
@@ -5,11 +5,17 @@ const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const pluginNavigation = require('@11ty/eleventy-navigation');
 const markdownIt = require('markdown-it');
 const markdownItAnchor = require('markdown-it-anchor');
+const pluginPWA = require("eleventy-plugin-pwa");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
+
+  eleventyConfig.addPlugin(pluginPWA, {
+    clientsClaim: true,
+    skipWaiting: true
+  });
 
   eleventyConfig.setDataDeepMerge(true);
 
--- src/_data/global.js
+++ src/_data/global.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "MAIN_TITLE_NAME": "Performance",
   "SECOND_TITLE_NAME": "Kit",
   "INTRO": "This collection of tutorials are based on my learnings from client projects. For each topic I will show you how to identify, implement and evaluate performance techniques that can improve your site's load time.",
@@ -6,5 +6,6 @@
   "BACK_TO_HOME_LINK_TEXT": "Back to home page",
   "HOME_URL_LINK": "Performance Kit - Home page",
   "AUTHOR": "Matthew Claffey",
-  "MADE_WITH": "Proudly Made With ❤️ by"
-}
+  "MADE_WITH": "Proudly Made With ❤️ by",
+  "BUILD_NUMBER": process.env.BUILD_ID
+};
